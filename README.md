4daed2f2d20c40c98dd23a0843492372a0577df3
acharya <sav.acharya22@gmail.com>
2024-04-25 11:39:37 +1000

    feat: localStorage retrieve and set implemented

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# ca-webproject
+# ca-webproject - Local Storage
+
+# Features
+
+1. Use local storage to persist CSS theme information
+2. Create css theme information to store
+    - CSS theme is array of object
+        - Objects are {name, hex}
+    - Export theme as code
+
+3. Professional lil touches on websites
+    - Title
+    - Favicon
+    - Decent CSS implementation
+            - Dark/light mode
+            - Mobile first design and flexbox
+    - External font from Google fonts
+
+
+## Nice to haves
+
+1. Exampls of CSS themes generated applied to HTML elements/components
+2. Calendar & date to timestamp  CSS theme
+3. Donate/sponsorship button in website  & github repo
---
+++ src/scripts/dataManager.js
@@ -0,0 +1,42 @@
+
+
+// Declare JS data that we cwant to store
+
+let cssThemes = [
+
+]
+
+let pageTheme = "dark"
+
+
+/**
+ * Retrieve, assign and return the latest stored css theme  list from the browser localStorage
+ * @returns Array of objects
+ */
+function getStoredCssTheme() {
+    // update the csstheme array
+    let rawJsonStringCssThemes = localStorage.getItem('cssThemes');
+    cssThemes = JSON.parse(rawJsonStringCssThemes);
+    //  return the update data
+    return cssThemes
+}
+
+/**
+ * Retrieve, assign and return the latest stored page theme from the browser localStorage
+ * @returns String. Should be light or dark
+ */
+
+function getStoredPageTheme() {
+    pageTheme = localStorage.getItem("pageTheme")
+    return pageTheme
+}
+
+
+//  Create/update localstorage
+
+function setCssThemeToStorage() {
+    let dataAsJsonString = JSON.stringify(cssThemes)
+    localStorage.setItem("cssThemes", dataAsJsonString)
+}
+
+//  Delete localstorage
